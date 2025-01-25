2436cc4169c85820a3630e7c4d5b0ef4f3041f2d
sg4HXuoa4XHfE4 <basest.annoy.4f@icloud.com>
2025-01-25 15:31:42 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  optimization: {
+    minimize: false
+  },
+  entry: {
+    popup: './popup/popup.jsx'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'popup'),
+    environment: {
+      // The environment supports arrow functions ('() => { ... }').
+      arrowFunction: true,
+      // The environment supports BigInt as literal (123n).
+      bigIntLiteral: false,
+      // The environment supports const and let for variable declarations.
+      const: true,
+      // The environment supports destructuring ('{ a, b } = obj').
+      destructuring: true,
+      // The environment supports an async import() function to import EcmaScript modules.
+      dynamicImport: false,
+      // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+      forOf: true,
+      // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+      module: false,
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-react', {
+                runtime: 'automatic'
+              }],
+              ['@babel/preset-env', {
+                targets: {
+                  chrome: "58"
+                }
+              }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['tailwindcss', {}],
+                  ['autoprefixer', {}]
+                ]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
